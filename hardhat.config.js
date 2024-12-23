03cef786ae746d1857043d48647f0470878727fa
Anush <anushshinde2019@gmail.com>
2024-12-23 17:51:16 +0530

    Deploying to remote networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`, // Replace with your Infura project ID
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Replace with your private key
+    },
+  },
   solidity: "0.8.28",
 };
